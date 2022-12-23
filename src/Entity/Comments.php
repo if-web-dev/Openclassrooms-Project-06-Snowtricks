b862618ec30827bbe335d7de04d4187deb739952
ishake fouhal <i.fouhal@hotmail.com>
2022-12-23 16:28:56 +0100

    close #6, add basic registration feature + css fixes

--- src/Entity/Comments.php
+++ src/Entity/Comments.php
@@ -72,6 +72,10 @@ class Comments
         return $this;
     }
 
+    public function __construct() {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
