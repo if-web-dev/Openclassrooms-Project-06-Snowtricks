182e694f0f26f6d719dd96126fc4effef0342374
ishake fouhal <i.fouhal@hotmail.com>
2023-01-04 21:57:06 +0100

    close #11, add home page pagination + video thumbnail display fixes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', 'home.index', methods: ['GET'])]
-    public function index(TrickRepository $tricksRepository): Response
+    public function index(TrickRepository $tricksRepository, Request $request): Response
     {
-        return $this->render('home.html.twig', [ 'tricks' => $tricksRepository->findAll()]);
+        $page = $request->query->getInt('page', 1);
+        
+        $tricks = $tricksRepository->findTricksPaginated($page, 5);
+
+        //return $this->render('home.html.twig', [ 'tricks' => $tricksRepository->findAll()]);
+
+        return $this->render('home.html.twig', [ 'tricks' => $tricks ]);
+
     }
 }
 
