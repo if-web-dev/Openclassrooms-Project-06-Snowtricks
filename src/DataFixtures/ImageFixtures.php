282e5f1dbd400a41db962128b7e1b4939679532f
ishake fouhal <i.fouhal@hotmail.com>
2023-01-20 00:01:50 +0100

    close #28, add Image and Video Entity + their fixtures

---
+++ src/DataFixtures/ImageFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Image;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class ImageFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $Medias =['jump1.jpg','jump2.jpg','jump3.jpg'];
+
+        //for 10 Trick
+        for($i=0; $i<=9; $i++){
+
+            //add 2 images random
+
+            $trick = $this->getReference(TrickFixtures::TRICK_REFERENCE . $i);
+
+            for ($j = 0; $j <= 1; $j++){
+
+                $imagePath = $Medias[rand(0,2)];
+
+                $image = new Image();
+
+                $image->setPath($imagePath)
+                      ->setTrick($trick);
+
+                $manager->persist($image);
+
+            }
+
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+       return [
+            UserFixtures::class,
+            TrickFixtures::class,
+        ];
+    }
+}
---
+++ src/DataFixtures/VideoFixtures.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Video;
+use App\Service\YoutubeThumbnail;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class VideoFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+      
+        $media = [
+
+            'https://www.youtube.com/watch?v=5ylWnm4rF1o',
+
+
+            'https://www.youtube.com/watch?v=4vGEOYNGi_c',
+
+
+            'https://www.youtube.com/watch?v=TTgeY_XCvkQ'
+
+        ];
+
+        //for 10 Tricks
+        for ($i = 0; $i <= 9; $i++) {
+
+            $trick = $this->getReference(TrickFixtures::TRICK_REFERENCE . $i);
+
+            //add 2 videos random 
+
+            for ($j = 0; $j <= 1; $j++) {
+
+                $videoUrl = $media[rand(0, 2)];
+
+                $thumbnail = YoutubeThumbnail::getThumbnail($videoUrl);
+
+                $video = new Video();
+                $video->setUrl($videoUrl)
+                    ->setTrick($trick)
+                    ->setThumbnail($thumbnail);
+
+                $manager->persist($video);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+            TrickFixtures::class,
+        ];
+    }
+}
